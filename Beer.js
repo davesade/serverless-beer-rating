7b8ec8e7b692bd44be70727142a56eccfaea1a0d
Pedro Marquez <pedro.f.marquez.soto@accenture.com>
2017-02-11 00:33:22 -0600

    Added first version

---
+++ Beer.js
@@ -0,0 +1,80 @@
+'use strict';
+
+class Beer{
+	constructor(dbClient, uuid){
+		this.docClient = dbClient;
+		this.uuid = uuid;
+	}
+	hello(event,callback){
+		const response = {
+			statusCode: 200,
+			body: JSON.stringify({
+			  message: 'Go Serverless v1.0! Your function executed successfully!',
+			  input: event,
+			}),
+		};
+
+		callback(null, response);
+	}
+	addRating(params,callback){
+		  var Item = {
+			  id: uuid.v4(),
+			  beer: params.beer,
+			  rating: Number(params.rating),
+			  description: params.description
+		  };
+
+		  docClient.put({TableName: 'slsbeer', Item: Item}, (error) => {
+			if (error) {
+			  callback(error);
+			}
+
+			callback(null, {
+			  statusCode: 201,
+			  headers: {
+				'Access-Control-Allow-Origin': '*'
+			  },
+			});
+		  });
+	}
+	getRating(params,callback){
+	  docClient.scan(params, (error, data) => {
+		if (error) {
+		  callback(error);
+		}
+
+		var sum = data.Items.reduce((accumulated, current) => {
+		  return accumulated + current.rating}
+		, 0);
+
+		var average = sum/data.Items.length;
+
+		callback(null, {
+		  statusCode: 200,
+		  headers: {
+			'Access-Control-Allow-Origin': '*'
+		  },
+		  body: JSON.stringify({ averageRating: average }),
+		})
+
+	  });
+	}
+	allRatings(params,callback){
+		docClient.scan(params, (error, data) => {
+			if (error) {
+			  callback(error);
+			}
+
+			callback(null, {
+			  statusCode: 200,
+			  headers: {
+				'Access-Control-Allow-Origin': '*'
+			  },
+			  body: JSON.stringify(data.Items),
+			})
+
+		  });
+	}
+}
+
+module.exports = Beer;
